bf80102e14fcb0d024059e0084029dec0a9c3be9
Kadir <84664117+VectortheGreat@users.noreply.github.com>
2025-10-23 09:36:57 +0300

    project init

---
+++ src/test/java/com/autumnus/spring_boot_starter_template/SpringBootStarterTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autumnus.spring_boot_starter_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
