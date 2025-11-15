ab689ffe9cb97295402797921def8ea64fc65369
Kadir <84664117+VectortheGreat@users.noreply.github.com>
2025-11-15 21:10:54 +0300

    record builder error fixed

--- src/main/java/com/autumnus/spring_boot_starter_template/common/messaging/dto/NotificationMessage.java
+++ src/main/java/com/autumnus/spring_boot_starter_template/common/messaging/dto/NotificationMessage.java
@@ -1,22 +1,20 @@
 package com.autumnus.spring_boot_starter_template.common.messaging.dto;
 
-import com.autumnus.spring_boot_starter_template.common.messaging.dto.NotificationMessage.NotificationType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class NotificationMessage {
-
     Long userId;
     String title;
     String message;
     NotificationType type;
 
     public enum NotificationType {
-        INFO,
-        WARNING,
-        SUCCESS,
-        ERROR
+        SUCCESS, INFO, WARNING, ERROR
     }
 }
